de061dd9c36089cd40afbbf2c8664a36d51bc73b
Pritam Dutta <pritamdutta@Pritams-MacBook-Air.local>
2020-01-25 20:55:21 +0530

    first commit

---
+++ src/main/java/test/automation/FirstTest.java
@@ -0,0 +1,25 @@
+package test.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+
+	
+	
+	public static void main(String args[]) throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+	
+	    WebDriver driver = new ChromeDriver();
+
+	    driver.get("http://google.com");
+	     
+	    Thread.sleep(2000);
+	    
+        driver.quit();
+	}
+	
+}
